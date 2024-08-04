f71e9bc8b320755b3ee0a8f88159d610b34e5927
Aman Tiwari <111676086+AmanTiwari404@users.noreply.github.com>
2024-08-04 19:02:22 +0530

    Add files via upload

---
+++ Minimum Time/solution.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTime(int n, vector<int> &time, vector<int> &search, int k) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        deque<int> deq;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1] + time[i-1];
+            for (int j = 1; j <= k && i - j >= 0; j++) {
+                dp[i] = min(dp[i], dp[i-j] + search[i-j]);
+            }
+        }
+        return dp[n];
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> time(n);
+        for (int i = 0; i < n; i++) {
+            cin >> time[i];
+        }
+
+        vector<int> search(n);
+        for (int i = 0; i < n; i++) {
+            cin >> search[i];
+        }
+
+        int k;
+        cin >> k;
+
+        Solution obj;
+        int res = obj.minTime(n, time, search, k);
+
+        cout << res << endl;
+    }
+    return 0;
+}
